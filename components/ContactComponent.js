0e301f32694c4553eb8a591cbbb4b323155eed42
seanlawless <seanklawless76@gmail.com>
2021-03-25 17:01:28 -0700

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
-class Contact extends Component {
 
+export default class Contact extends Component {
+    
     static navigationOptions = {
-        title: 'Contact Us'
+        title: 'Contact us'
+    }
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        })
     }
 
     render() {
@@ -14,18 +24,31 @@ class Contact extends Component {
             <ScrollView>
                 <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
                     <Card
-                        title="Contact Information"
-                        wrapperStyle={{margin: 20}}>
+                    title='Contact Information'
+                    wrapperStyle={{margin: 20}}
+                    wrapperStyle={{marginBottom: 10}}>
                         <Text>1 Nucamp Way</Text>
                         <Text>Seattle, WA 98001</Text>
-                        <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                        <Text>U.S.A.</Text>
+
                         <Text>Phone: 1-206-555-1234</Text>
                         <Text>Email: campsites@nucamp.co</Text>
+                        <Button
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                            icon={<Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{marginRight: 10}}
+                            />}
+                            onPress={() => this.sendMail()}
+                        />
                     </Card>
                 </Animatable.View>
             </ScrollView>
-        );
-    };
+        )
+    }
 }
 
 export default Contact;
